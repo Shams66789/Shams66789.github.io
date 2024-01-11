7c1f18f4b6e8da638c1c65551325391c8925f044
Shams66789 <rajansari66789@gmail.com>
2024-01-11 23:40:52 +0530

    project section design

--- script.js
+++ script.js
@@ -12,20 +12,20 @@ $(document).ready(function(){
     })
 })
 
-var myIndex = 0;
-carousel();
+// var myIndex = 0;
+// carousel();
 
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 2500);    
-}
+// function carousel() {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";  
+//   }
+//   myIndex++;
+//   if (myIndex > x.length) {myIndex = 1}    
+//   x[myIndex-1].style.display = "block";  
+//   setTimeout(carousel, 2500);    
+// }
 
 function goToSection() {
     window.location.hash = "skills";
