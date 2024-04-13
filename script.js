e845f3efe112dc1fef7edc812040456cb9ca8762
Shams66789 <rajansari66789@gmail.com>
2024-04-13 16:11:46 +0530

    Designing work of most pages completed

--- script.js
+++ script.js
@@ -1,16 +1,60 @@
-$(document).ready(function(){
-    $(window).scroll(function () {
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
+// $(document).ready(function(){
+//     $(window).scroll(function () {
+//         if(this.scrollY >= 20){
+//             $('.navbar').addClass("sticky");
+//         }else{
+//             $('.navbar').removeClass("sticky");
+//         }
+//     })
+//    
+// })
+
+
+// $('.menu-btn').click(function () {
+//     console.log('clicicicicici');
+//     $('.navbar .menu').toggleClass('active');
+//     $('.menu-btn i').toggleClass('active');
+// })
+const addScrollEvent = () => 
+{
+    const handleScroll = (e) => {
+        const navbar = document.querySelector(".navbar")
+        const { scrollY } = window
+
+        if (scrollY >= 40 ) {
+            navbar.classList.add("sticky")
+        } else {
+            navbar.classList.remove("sticky")
+
         }
+    }
+
+    document.addEventListener('scroll', handleScroll)
+}
+
+const addMenuBtnClickListener = () => 
+{
+    const menuBtn = document.querySelector("div.menu-btn")
+
+    console.log({menuBtn});
+
+    menuBtn.addEventListener('click', () => {
+        console.log("clicked ");
+        document.querySelector(".navbar .menu").classList.toggle('active')
+        document.querySelector(".menu-btn i").classList.toggle('active')
+
     })
-    $('.menu-btn').click(function () {
-        $('.navbar .menu').toggleClass('active');
-        $('.menu-btn i').toggleClass('active');
-    })
-})
+}
+
+
+
+
+addScrollEvent()
+addMenuBtnClickListener()
+
+
+
+
 
 // var myIndex = 0;
 // carousel();
